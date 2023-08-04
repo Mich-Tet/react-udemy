60d7bae5dbd1edb3a4983a807bf5e83016ac261e
mich-Tet <72336908+Mich-Tet@users.noreply.github.com>
2023-08-04 11:09:39 +0200

    module-13

--- module-13/src/App.js
+++ module-13/src/App.js
@@ -5,17 +5,23 @@ import DemoOut from './components/UI/Button/Demo/DemoOut';
 
 function App() {
   const [newPara, setNewPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('xd');
 
   const toggleHandler = useCallback(() => {
-    setNewPara((prevNewPara) => !prevNewPara);
-  }, []);
-
+    if (allowToggle) {
+      setNewPara((prevNewPara) => !prevNewPara);
+    }
+  }, [allowToggle]);
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOut show={false} />
+      <DemoOut show={newPara} />
+      <Button onClick={allowToggleHandler}>Allow</Button>
       <Button onClick={toggleHandler}>Show para</Button>
     </div>
   );
